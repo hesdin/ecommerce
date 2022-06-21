3da61e9663852eefe1581c154a834b7df64e853b
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-06-21 15:11:42 +0800

    logout response code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,7 +71,7 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'berhasil'
-        ]);
+        ], 200);
     }
 
     public function userRegister(Request $req)
